da1e85e5297250423cc9d005feb59c432fd3f640
Rob Mercer <robmercer@Robs-MBP.attlocal.net>
2021-01-27 09:54:37 -0500

    more cleanup

--- frontend/main.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-import React, { Component } from 'react';
-import {render} from 'react-dom';
-
-import routes from './routes';
-
-//render(App(), document.getElementById('app'));
-render(routes, document.getElementById('app'));
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,3 @@
-'use strict'
-import React, { Component } from 'react';
 import {render} from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';      
 
