63f8a0c76a149a9e59f48320f5faf3d6abdbf8d0
Rob Mercer <robmercer@Robs-MacBook-Pro.local>
2021-03-14 21:11:58 -0400

    display complete/incomplete on card level

--- frontend/src/CardName.js
+++ frontend/src/CardName.js
@@ -1,22 +1,36 @@
 import {Link} from "react-router-dom";
 
+const CompleteCheckBox = (item) => {
+    return (
+        <input className={`mr-2`} 
+            type="checkbox" 
+            id={item.item.id} 
+            name="complete" 
+            defaultChecked={item.item.completed ? "true" : ''}
+            onChange={() => {}}
+            value="complete"></input>
+    );
+}
+
 const CardName = (item) => {
         if (item.item.editEnabled) {
         return (
-        <input type="text" 
-            style={{marginRight: '10px', width: '70%' }}
-            defaultValue={item.item.description} 
-            onChange={e => item.setCardNameCallback(item.item.id, e.target.value)}
-            />
+            <span
+                style={{marginRight: '10px', width: '79%' }}
+                className={`todo-title mr-2 completed-todo`}>
+                <CompleteCheckBox item={item} />
+                <input type="text" 
+                    style={{marginRight: '10px', width: '90%' }}
+                    defaultValue={item.item.description} 
+                    onChange={e => item.setCardNameCallback(item.item.id, e.target.value)}
+                    />
+            </span>
         );
         } else {
         return (
         <span
-            /*className={`todo-title mr-2 ${
-            this.state.viewCompleted ? "completed-todo" : ""
-            }`}*/
-            className={`todo-title mr-2 completed-todo`}
-        >
+            className={`todo-title mr-2 completed-todo`}>
+            <CompleteCheckBox item={item} />
             {item.item.description}
         </span>
         );
